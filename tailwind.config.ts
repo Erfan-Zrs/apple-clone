a21aa3a97647aa65d2c8eef8f4dc26845b382e6d
lock-ezr <erfan.dev.lock@gmail.com>
2025-01-11 17:06:40 +0330

    added assets and set up project for development

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,10 +5,19 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
+        blue: "#2997FF",
+        gray: {
+          DEFAULT: "#86868b",
+          100: "#94928d",
+          200: "#afafaf",
+          300: "#42424570",
+        },
+        zinc: "#101010",
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
